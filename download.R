b7f2d2b78da11541822066a3d20482f3dd3ae963
Stewart Smith <sms@pccl.org.uk>
2020-03-21 18:05:21 +0000

    Add tidy code.

--- download.R
+++ download.R
@@ -5,7 +5,7 @@ power.download <- function() {
     zipUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
     
     # Output files
-    timeFile    <- "DownloadTime.csv"
+    timeFile    <- "DownloadTime.txt"
     zipFile     <- "DataSet.zip"
 
     # Download the Project ZIP file
@@ -13,10 +13,28 @@ power.download <- function() {
     download.file(zipUrl, zipFile, mode = "wb", quiet = TRUE)
     
     # Save the time
-    write.csv(Sys.time(), timeFile);
+    write.table(Sys.time(), timeFile, col.names = FALSE, row.names = FALSE)    
     
     # Unzip the data.
     message("Unzipping Dataset")
     unzip(zipFile) 
 }
+
+power.tidy <- function() {
+
+    sourceFile <- "household_power_consumption.txt"
+    dateFormat <- "%d/%m/%Y"
     
+    message("Reading Full Dataset")
+    table <- read.table(sourceFile, header=TRUE, sep=";", na.strings = "?")
+
+    message("Triming Dataset")
+    # Make sure the date is in the correct format
+    table$Date <- as.Date(table$Date, dateFormat)
+    
+    # We're only interested in 2 days of data
+    table <- subset(table ,(Date >= as.Date("1/2/2007", dateFormat))
+                    & (Date <= as.Date("2/2/2007", dateFormat)))
+    
+    table
+}
